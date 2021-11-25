3bfc5175553c6fb56a9c4604943a847a5473bb06
pillepaal <pillepaal@gmail.com>
2021-11-25 15:12:52 +0000

    "first commit"

---
+++ WeCanDoItApplication.java
@@ -0,0 +1,13 @@
+package com.pilleriina.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCanDoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeCanDoItApplication.class, args);
+	}
+
+}
